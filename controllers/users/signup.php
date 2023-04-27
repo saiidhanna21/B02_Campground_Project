0a046048c41f7f7b4e275d7ca59a0c610fb517ec
saiidhanna21 <hanna.saiid2003@gmail.com>
2023-04-27 10:28:35 +0300

    styling update

--- controllers/users/signup.php
+++ controllers/users/signup.php
@@ -23,13 +23,20 @@ if (empty($_POST)) {
     $query1 = "SELECT * FROM `users` WHERE `email`='$email'";
     $result1 = mysqli_query($con, $query1);
     
+    $pattern = "/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+-=])[a-zA-Z0-9!@#$%^&*()_+-=]{8,}$/";
     if (mysqli_fetch_array($result)) {
         $arr["username"] = "";
         echo flash('Username already exists!', true);
+    } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $arr["email"] = "";
+        echo flash('Not a valid email', true);
     } else if (mysqli_fetch_array($result1)) {
         $arr["email"] = "";
         echo flash('Email already exists! Want to <a href="login.php">Log In</a>?', true);
-    } else {
+    } else if(!preg_match($pattern,$password)){
+        $arr["password"] = "";
+        echo flash('Password must consist of at least 8 characters, including one uppercase letter and one symbol (e.g. ! @ # $ % ^ & * ( ) _ + - =)', true);
+    }else {
         $_SESSION["username"] = $username;
         $hashed = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO `users` (`username`, `password`, `firstName`, `lastName`, `phoneNumber`, `email`)
